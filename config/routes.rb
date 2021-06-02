168a5ab7f0c397ba7d9435a215d820ad8e2c395c
tony <t_tony_182@yahoo.com>
2021-06-02 09:15:38 +0300

    default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # install the default route as the lowest priority.
+  match '/:controller(/:action(/:id))', via: :all
+  # match ':controller/:action.:format'
+  match '*anything' => 'error#unhandled_request', via: :all
 end
