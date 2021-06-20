aac6988a8fc64d8b37834a522dac45705bfabd0b
tony <t_tony_182@yahoo.com>
2021-06-20 13:15:33 +0300

    hide variables

--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,10 @@
 require_relative 'application'
 
 ActionMailer::Base.smtp_settings = {
-  :port           => 587,
-  :address        => 'smtp.mailgun.org',
-  :user_name      => 'postmaster@sandbox26fb8ed5edd543eaa39b46b0aa172029.mailgun.org',
-  :password       => 'dff60577e52a7c129299c7e2e4c6d77e-24e2ac64-ae8a8af9',
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'waterplanto.heroku.com',
   :authentication => :plain,
 }
