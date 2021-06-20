9e763d5bf393dda75e6466cf8ddd311e0c43d59b
tony <t_tony_182@yahoo.com>
2021-06-20 11:21:40 +0300

    try emailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.smtp_settings = {
+  :port           => 465,
+  :address        => 'mail.tonydesign.ro',
+  :user_name      => 'test@tonydesign.ro',
+  :password       => 'fFg0%0d$zj2F',
+  :domain         => 'waterplanto.heroku.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
+
 # Initialize the Rails application.
 Rails.application.initialize!
